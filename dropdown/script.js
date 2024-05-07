634f7b534a929173fde2dacc7c2d688903d342b9
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-05-06 21:48:37 -0400

    Refactor CSS to properly display dropdown contents.

--- dropdown/script.js
+++ dropdown/script.js
@@ -1,7 +1,7 @@
 class Dropdown {
   constructor(buttonSelector, itemsSelector) {
-    this.button = document.querySelector(buttonSelector);
-    this.items = document.querySelector(itemsSelector);
+    this.button = document.getElementById(buttonSelector);
+    this.items = document.getElementById(itemsSelector);
 
     this.button.addEventListener('click', () => {
       this.toggleDropdown();
@@ -9,14 +9,25 @@ class Dropdown {
   };
 
   toggleDropdown() {
+    if (document.querySelector('.showing')) {
+      let activeDropdown = document.querySelector('.showing');
+      activeDropdown.classList.remove('showing');
+      activeDropdown.previousElementSibling.classList.remove('active-dropdown');
+    };
     this.items.classList.toggle('showing');
+    this.button.classList.toggle('active-dropdown');
   };
 };
 
-const myDropdown = new Dropdown('.dropdown-button', '.dropdown-content');
+const business = new Dropdown('business-button', 'business');
+const cooking = new Dropdown('cooking-button', 'cooking');
 
 window.addEventListener('click', (e) => {
-  if (e.target != myDropdown.button && myDropdown.items.classList.contains('showing')) {
-    myDropdown.toggleDropdown();
+  if (!e.target.matches('.active-dropdown')) {
+    let activeDropdown = document.querySelector('.showing');
+    if (activeDropdown) {
+      activeDropdown.classList.remove('showing');
+      activeDropdown.previousElementSibling.classList.remove('active-dropdown');
+    };
   };
 });
